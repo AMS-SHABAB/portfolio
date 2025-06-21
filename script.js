ae96d6722748cbce633e7b4f7cad67a2987e88ad
Muktadir Shabab <muktadirsabab21@gmail.com>
2025-06-21 22:27:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+  const image = document.getElementById("dynamicImage");
+  const effects = ["grayscale", "cyberpink", "original"];
+  let index = 0;
+
+  setInterval(() => {
+    image.className = "fixed-image " + effects[index];
+    index = (index + 1) % effects.length;
+  }, 5000);
